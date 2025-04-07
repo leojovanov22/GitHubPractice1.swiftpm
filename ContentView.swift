79e998ac94eee79caedce2ced32b3a603b383fcf
Will Baik <wbaik7022@stu.d214.org>
2025-04-07 11:33:31 -0700

    Added a textfield

--- ContentView.swift
+++ ContentView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var name1 = ""
+    @State var age1 = ""
     var body: some View {
         Text("Will")
             .foregroundStyle(.white)
@@ -13,7 +14,8 @@ struct ContentView: View {
             .font(.custom("Chalkduster", size: 35))
             .shadow(radius: 10)
         
-        TextField("Enter a Name", text: $name1)
+        TextField("Enter Your Name", text: $name1)
+        TextField("Enter Your Age", text: $age1)
         Button {
             
         } label: {
