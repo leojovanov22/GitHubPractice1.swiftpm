ac9eac6b7b5ffb50b54d32851aca0979f9a29493
Will Baik <wbaik7022@stu.d214.org>
2025-04-07 11:37:09 -0700

    Birthday

--- ContentView.swift
+++ ContentView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct ContentView: View {
     @State var name1 = ""
     @State var age1 = ""
+    @State var birthday1 = ""
     var body: some View {
         Text("Will")
             .foregroundStyle(.white)
@@ -16,11 +17,14 @@ struct ContentView: View {
         
         TextField("Enter Your Name", text: $name1)
         TextField("Enter Your Age", text: $age1)
+        TextField("Enter Your Birthday", text: $birthday1)
         Button {
             
         } label: {
             Text("Click Me")
         }
+        
+        
 
 
     }
